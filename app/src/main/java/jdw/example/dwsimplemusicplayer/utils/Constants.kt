0eb1fde88d1d588160307651de2f07a83164e3b4
pjeong1024 <peterjeong1024@gmail.com>
2024-07-24 22:38:37 -0700

    remove specific key values

--- app/src/main/java/jdw/example/dwsimplemusicplayer/utils/Constants.kt
+++ app/src/main/java/jdw/example/dwsimplemusicplayer/utils/Constants.kt
@@ -1,8 +1,6 @@
 package jdw.example.dwsimplemusicplayer.utils
 
 object Constants {
-    // https://api.openweathermap.org/data/2.5/forecast/daily?q=lisbon&appid=ed60fcfbd110ee65c7150605ea8aceea&units=imperial
-    // https://api.openweathermap.org/data/2.5/forecast?q=London&appid=eef25b7b87eaf1a47877a633d3dd66c7
     const val BASE_URL = "https://api.openweathermap.org/"
     const val API_KEY = "ed60fcfbd110ee65c7150605ea8aceea"
 }
